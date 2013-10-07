1b50762f11d1aac0b3f83641a19025e905763f44
Christopher Anderton <chris@tikidream.com>
2013-10-07 14:24:54 -0400

    Improvements to widget collapse

--- assets/js/responsive-sidebar.js
+++ assets/js/responsive-sidebar.js
@@ -12,25 +12,51 @@ jQuery(document).ready(function($){
 		
 		// If it's smaller than the min_width send it to the navbar, otherwise send it back.
 		if($(window).width() <= min_width) {
-			console.debug('Moving '+node_id+' to '+nav_id+' because window width '+$(window).width()+' is less than '+min_width);
 			
 			// Create the collapse button if necessary
 			if($('#collapse-'+node_id).length == 0) {
-				var btn_collapse = '<button id="collapse-'+node_id+'" type="button" class="navbar-toggle" data-toggle="collapse" data-target=".nav-'+node_id+'"><span class="sr-only">Toggle asides</span><i class="icon-th-large"></i>?</button><a class="btn" href="#"><i class="icon-align-left"></i></a>';
+				var btn_collapse = '<button id="collapse-'+node_id+'" type="button" class="navbar-toggle" data-toggle="collapse" data-target=".nav-'+node_id+'"><span class="sr-only">Toggle asides</span><i class="glyphicon-star-empty glyphicon"></i></button><a class="btn" href="#"><i class="icon-align-left"></i></a>';
 				$(btn_collapse).appendTo('#'+nav_id+' .navbar-header');
-				collapseBtnExists = true;
 			}
-			
-			// Move the stuff to the place
+
+			// Move the sidebar widgets to the navbar
 			if($('#'+nav_id+' #'+node_id).length == 0) {
-				$('<div class="nav-'+node_id+' collapse"><ul class="navbar-nav"></ul></div>').appendTo('#'+nav_id);
-				$('#'+node_id+' .widget').appendTo($('<li/>')).appendTo($('.nav-'+node_id+' ul'));
+				var navCollapse = $('<div class="nav-'+node_id+' collapse navbar-nav"/>').append($('<ul id="widgets-'+node_id+'" />'));
+				console.debug(navCollapse);
+				$('#nav-main').append(navCollapse);
+
+				// Change the nodes to <li>
+				$('#'+node_id+' .widget').changeElementType('li');
+
+				// Add them to the navbar. We have
+				$('#'+node_id+' .widget').each(function(){
+					$('#widgets-'+node_id).append( $(this) );
+				});
+
 			}
 		}
 		else {
 			console.debug('Moving '+node_id+' back because window width '+$(window).width()+' is greater than '+min_width);
 			$('.nav-'+node_id+' .widget').appendTo('#'+node_id);
-			//$('.nav-'+node_id+' ul li').append($('#'+node_id)).after('#primary');
+
+			// Change the nodes to <aside>
+				$('#'+node_id+' .widget').changeElementType('aside');
 		}
 	}
 });
+
+// Type change jQuery plugin
+(function($) {
+	$.fn.changeElementType = function(newType) {
+		console.debug('Changing element to '+newType);
+		var attrs = {};
+
+		$.each(this[0].attributes, function(idx, attr) {
+			attrs[attr.nodeName] = attr.nodeValue;
+		});
+
+		this.replaceWith(function() {
+			return $("<" + newType + "/>", attrs).append($(this).contents());
+		});
+	};
+})(jQuery);

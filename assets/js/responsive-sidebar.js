a41641381f794779771df777103cf8453826fb4c
Christopher Anderton <chris@tikidream.com>
2013-10-18 09:57:34 -0400

    Improvements to responsiveness

--- assets/js/responsive-sidebar.js
+++ assets/js/responsive-sidebar.js
@@ -6,25 +6,42 @@ jQuery(document).ready(function($){
 	});
 
 	function responsive_sidebar(){
-		node_id = 'secondary';
-		nav_id = 'nav-main';
-		min_width = 768;
+		// Node names
+		var node_id = 'secondary';
+		var nav_id = 'nav-main';
+		
+		// Bootstrap standard sizes
+		var screen_xs = 480;
+		var screen_sm = 768;
+		var screen_md = 992;
+		var screen_lg = 1200;
+
+		// Toggling width
+		var max_width = screen_lg;
+		var std_width = screen_xs;
 
 		$('.navbar-toggle').collapse('show');
-		
-		// If it's smaller than the min_width send it to the navbar, otherwise send it back.
-		if($(window).width() <= min_width) {
-			
+		// If it's larger than the standard width, send it to the normal nav instead
+			if($(window).width > std_width) {
+				var nav_target = $('<li class="btn-sidebar"/>').appendTo($('#'+nav_id+' .nav').last());
+				console.debug('Target: nav ul');
+			} else {
+				var nav_target = $('#'+nav_id+' .navbar-header');
+				console.debug('Target: navbar-header');
+			}
+
+		// If it's smaller than the max width create if required
+		if($(window).width() <= max_width) {
+
 			// Create the collapse button if necessary
 			if($('#collapse-'+node_id).length == 0) {
 				var btn_collapse = '<button id="collapse-'+node_id+'" type="button" class="navbar-toggle" data-toggle="collapse" data-target=".nav-'+node_id+'"><span class="sr-only">Toggle asides</span><i class="glyphicon-star-empty glyphicon"></i></button>';
-				$(btn_collapse).appendTo('#'+nav_id+' .navbar-header');
+				$(btn_collapse).appendTo(nav_target);
 			}
 
 			// Move the sidebar widgets to the navbar
 			if($('#'+nav_id+' #'+node_id).length == 0 && $('.nav-'+node_id).length == 0) {
 				var navCollapse = $('<div class="nav-'+node_id+' collapse navbar-nav"/>').append($('<ul id="widgets-'+node_id+'" />'));
-				console.debug(navCollapse);
 				$('#nav-main').append(navCollapse);
 
 				// Change the nodes to <li>
@@ -38,7 +55,6 @@ jQuery(document).ready(function($){
 			}
 		}
 		else {
-			console.debug('Moving '+node_id+' back because window width '+$(window).width()+' is greater than '+min_width);
 			$('.nav-'+node_id+' .widget').appendTo('#'+node_id);
 			$('.nav-'+node_id).remove();
 
@@ -51,7 +67,6 @@ jQuery(document).ready(function($){
 // Type change jQuery plugin
 (function($) {
 	$.fn.changeElementType = function(newType) {
-		console.debug('Changing element to '+newType);
 		var attrs = {};
 
 		$.each(this[0].attributes, function(idx, attr) {

e8bbe545e236acf3744350bdf83d27d0e1468815
Christopher Anderton <chris@tikidream.com>
2013-10-22 15:39:02 -0400

    Journal issue improvements

---
+++ page-issue.php
@@ -0,0 +1,36 @@
+<?php
+
+// This file is part of the Carrington Blueprint Theme for WordPress
+//
+// Copyright (c) 2008-2013 Crowd Favorite, Ltd. All rights reserved.
+// http://crowdfavorite.com
+//
+// Released under the GPL license
+// http://www.opensource.org/licenses/gpl-license.php
+//
+// **********************************************************************
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// **********************************************************************
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+
+get_header();
+?>
+
+<div id="primary" class="c1-8">
+<?php
+	$issue_id = $remoraOJS->get_requested_issue_id();
+
+	// If nothing else, fetch the current issue Table of Contents
+	$journal_page = $remoraOJS->get_issue_by_id($issue_id);
+	echo $remoraOJS->show_page($journal_page);
+	?>
+</div><!-- #primary -->
+
+<?php 
+get_sidebar();
+get_footer();
+?>
---
+++ page-journal.php
@@ -0,0 +1,44 @@
+<?php
+
+// This file is part of the Carrington Blueprint Theme for WordPress
+//
+// Copyright (c) 2008-2013 Crowd Favorite, Ltd. All rights reserved.
+// http://crowdfavorite.com
+//
+// Released under the GPL license
+// http://www.opensource.org/licenses/gpl-license.php
+//
+// **********************************************************************
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+// **********************************************************************
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+
+get_header();
+?>
+
+<div id="primary" class="c1-8">
+<?php
+	$article_id = $remoraOJS->get_requested_article_id();
+	$galley = $remoraOJS->get_requested_galley_type();
+
+	// Fetch the galley dom if a galley is requested	
+	if($article_id && $galley) $journal_page = $remoraOJS->get_galley_by_article_id($article_id, $galley);
+
+	// Or fetch the article DOM if there is just an article id
+	elseif($article_id) $journal_page = $remoraOJS->get_article_by_id($article_id);
+
+	// If nothing else, fetch the current issue Table of Contents
+	else $journal_page = $remoraOJS->get_issue_by_id('current');
+
+	echo $remoraOJS->show_page($journal_page);
+	?>
+</div><!-- #primary -->
+
+<?php 
+get_sidebar();
+get_footer();
+?>

dc7104082a5fea31d510c2e9afc6ce24169b794b
Christopher Anderton <chris@tikidream.com>
2013-10-22 12:34:39 -0400

    Ensure dropdowns will toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 jQuery(function($) {
 
+	// Ensure dropdowns will toggle
+	$('.dropdown-toggle').dropdown();
+
 	// Requires jquery.placeholder.min.js
 	// Add support for placeholder attribute in older browsers.
 	$('input[placeholder]').placeholder();
